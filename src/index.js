333c0ee589dde45bb3cf88f55b4b01f7b20463de
Crossroads30 <Crossroads30@mail.ru>
2023-08-11 09:19:51 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-// JS
-import './js/app'
+import { default as App } from "./js/app.js";
+window.onload = () => {
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const app = new App;
+}
 
-// SCSS
-// import './assets/scss/main.scss'
+
