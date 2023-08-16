8abfe14395d32ad95dcd976635a1309c1f94ba14
Crossroads30 <Crossroads30@mail.ru>
2023-08-16 14:33:26 +0300

    feat: add main js files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { default as App } from "./js/app.js";
-window.onload = () => {
-  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-  const app = new App;
-}
-
-
---
+++ src/videos.js
@@ -0,0 +1,5 @@
+import { default as App } from './js/app-files/app-videos.js';
+window.onload = () => {
+    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+    const app = new App();
+};
