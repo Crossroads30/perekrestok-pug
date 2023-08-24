ae8e5ae92111121d78f2a0954258cccdc64ba31f
Crossroads30 <Crossroads30@mail.ru>
2023-08-24 21:12:08 +0300

    feat: add body scroll lock

--- src/js/app-files/app-burger.js
+++ src/js/app-files/app-burger.js
@@ -1,19 +1,22 @@
 const logo = document.querySelector('.burger__line');
 const menu = document.querySelector('.burger-menu');
+const body = document.body
 
 export default class Burger {
-  constructor() {
-    this.burgerToOpen();
-    this.logo = logo;
-    this.menu = menu;
-  }
+    constructor() {
+        this.burgerToOpen();
+        this.logo = logo;
+        this.menu = menu;
+        this.body = body;
+    }
 
-  burgerToOpen() {
-    document.querySelector('.burger').addEventListener('click', (event) => {
-      if (event) {
-          this.logo.classList.toggle('burger__open');
-          this.menu.classList.toggle('burger-menu__open');
-      }
-  });
-  }
+    burgerToOpen() {
+        document.querySelector('.burger').addEventListener('click', (event) => {
+            if (event) {
+                this.logo.classList.toggle('burger__open');
+                this.menu.classList.toggle('burger-menu__open');
+                this.body.classList.toggle("lock");
+            }
+        });
+    }
 }
